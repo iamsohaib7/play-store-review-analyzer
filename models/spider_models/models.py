bb2c0de68b01365a74c7f8656a9e6331d403ee95
Sohaib <sohaibt884@gmail.com>
2024-11-24 17:36:32 +0500

    add: app reviews model in spider_models/models.py

--- models/spider_models/models.py
+++ models/spider_models/models.py
@@ -1,10 +1,11 @@
 # this includes the models for storing the scrapped data in database,
 # it is considered as raw storage for scraped data.
-
+import uuid
 from datetime import datetime
+from typing import List
 
-from sqlalchemy import DateTime, String, Text
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import DateTime, String, Text, create_engine, ForeignKey, Integer, Uuid
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker
 
 
 class BaseModel(DeclarativeBase):
@@ -20,6 +21,44 @@ class AppInfoModel(BaseModel):
     added_at: Mapped[datetime] = mapped_column(
         DateTime, nullable=False, default=datetime.now
     )
+    reviews: Mapped[List["AppReviewsModel"]] = relationship(
+        "AppReviewsModel", back_populates="app_info", cascade="all, delete-orphan"
+    )  # App Info has one-to-many relationship with App reviews it shows the relationship
 
     def __repr__(self):
         return f"{self.app_id=}, {self.app_name=}, {self.url=}, {self.added_at=}"
+
+
+class AppReviewsModel(BaseModel):
+    __tablename__ = "app_reviews"
+    app_id: Mapped[str] = mapped_column(ForeignKey("app_info.app_id"))
+    review_id: Mapped[Uuid] = mapped_column(Uuid, nullable=False, primary_key=True, default=uuid.uuid4)
+    user_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=True)
+    app_info: Mapped["AppInfoModel"] = relationship(
+        "AppInfoModel", back_populates="reviews"
+    )
+    review_meta: Mapped["ReviewMetaDetailsModel"] = relationship(
+        "ReviewMetaDetailsModel",
+        back_populates="review",
+        uselist=False,
+        cascade="all, delete-orphan",
+    )
+
+
+class ReviewMetaDetailsModel(BaseModel):
+    __tablename__ = "review_meta_details"
+    _id: Mapped[Integer] = mapped_column(
+        Integer, nullable=False, primary_key=True, autoincrement=True
+    )
+    review_id: Mapped[Uuid] = mapped_column(Uuid, ForeignKey("app_reviews.review_id"))
+    ratings: Mapped[str] = mapped_column(Integer, nullable=False)
+    review_created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    thumbs_up_count: Mapped[Integer] = mapped_column(Integer, nullable=False, default=0)
+    review_created_version: Mapped[str] = mapped_column(String(100), nullable=True)
+    review_reply: Mapped[str] = mapped_column(Text, nullable=True)
+    reply_time: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    current_app_version: Mapped[str] = mapped_column(String(100), nullable=True)
+    review: Mapped["AppReviewsModel"] = relationship(
+        "AppReviewsModel", back_populates="review_meta"
+    )
